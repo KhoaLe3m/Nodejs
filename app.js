03bdccf6f8522adff1e26a60d30c26378d8c0588
dieutrinh28 <trinhdo.work@gmail.com>
2022-09-25 11:48:05 +0700

    add-cors

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ import homeRoute from "./routes/home";
 import productRoute from "./routes/products";
 import categoryRoute from "./routes/categories";
 import mongoose from "mongoose";
+import cors from "cors";
 
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(homeRoute);
@@ -18,7 +20,7 @@ mongoose.connect("mongodb+srv://root:8wWJwT6dnuqJQo3r@cluster0.swd4jkl.mongodb.n
     .then(()=>console.log("Kết nối db thành công"))
     .catch((error)=>console.log(error));
 // Bước 3: Lắng nghe cổng thực thi
-const port = 3001;
+const port = 8000;
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
 });
